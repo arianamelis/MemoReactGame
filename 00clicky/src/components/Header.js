dcb50e425c3e988e1f37e533a58eb446e6de964f
Ariana Melis <arianamelis@gmail.com>
2019-04-28 20:42:38 -0700

    content change

--- 00clicky/src/components/Header.js
+++ 00clicky/src/components/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
                 <div className='headerContainer'>
                     <h1>Daily Memory Game!</h1>
                     <h2>Challenge yourself. </h2>
-                    <h3>Can you remember the most important people of the 21 century?</h3>
+                    <h3>Can you remember the most influential people of the 21 century?</h3>
                     <p>Work your brain out. Click once. Click the same person twice to loose</p>
                 </div>
             </header>
