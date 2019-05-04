cfd6d4ff41e93a56c750851f918b1a210179f5a1
Ariana Melis <arianamelis@gmail.com>
2019-05-04 12:50:33 -0700

    Rearranged folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Game from "./components/game/game.js";
 import LandingPage from "./components/Landingpage/Landingpage.js";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
