c8b700226394033010ecb860904d7d2942c53d5e
Ariana Melis <arianamelis@gmail.com>
2019-05-04 12:10:56 -0700

    Stuff

---
+++ 00clicky/build/precache-manifest.efff2d4dd21e1dfc779bdc1d2ad21492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065c3c9a83304e8a018f234ec12fbe71",
+    "url": "/CLICKMEMORYREACTGAME/index.html"
+  },
+  {
+    "revision": "f5413d51f2c91e2cea0e",
+    "url": "/CLICKMEMORYREACTGAME/static/css/main.53ec1332.chunk.css"
+  },
+  {
+    "revision": "c096d120b82894ccdd37",
+    "url": "/CLICKMEMORYREACTGAME/static/js/2.9a3beece.chunk.js"
+  },
+  {
+    "revision": "f5413d51f2c91e2cea0e",
+    "url": "/CLICKMEMORYREACTGAME/static/js/main.fbd7dbd4.chunk.js"
+  },
+  {
+    "revision": "df76424df0c663465614",
+    "url": "/CLICKMEMORYREACTGAME/static/js/runtime~main.2dcfe83e.js"
+  }
+]);
