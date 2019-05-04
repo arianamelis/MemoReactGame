176d6e0c5b13097bed8fbcdfa527258d728b54db
Ariana Melis <arianamelis@gmail.com>
2019-05-04 13:15:28 -0700

    Updates

---
+++ precache-manifest.8626e2a6c48a475a610e055837d0de6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5802e95a34daf2d68223fb3dc4a301cf",
+    "url": "/MemoReactGame/index.html"
+  },
+  {
+    "revision": "2a01cfff67f90640754e",
+    "url": "/MemoReactGame/static/css/main.53ec1332.chunk.css"
+  },
+  {
+    "revision": "08f38146243a8cb25485",
+    "url": "/MemoReactGame/static/js/2.a474aea2.chunk.js"
+  },
+  {
+    "revision": "2a01cfff67f90640754e",
+    "url": "/MemoReactGame/static/js/main.4aa35989.chunk.js"
+  },
+  {
+    "revision": "a998ba369147baf89639",
+    "url": "/MemoReactGame/static/js/runtime~main.a51cba8c.js"
+  }
+]);
